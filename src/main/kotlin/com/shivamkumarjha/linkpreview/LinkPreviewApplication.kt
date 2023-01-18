44c3e07cd8f51b545d23c2e5a4fd123b95354d86
ShivamKumarJha <jha.shivam3@gmail.com>
2023-01-18 16:07:29 +0530

    Initial empty project
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

---
+++ src/main/kotlin/com/shivamkumarjha/linkpreview/LinkPreviewApplication.kt
@@ -0,0 +1,11 @@
+package com.shivamkumarjha.linkpreview
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LinkPreviewApplication
+
+fun main(args: Array<String>) {
+	runApplication<LinkPreviewApplication>(*args)
+}
---
+++ src/test/kotlin/com/shivamkumarjha/linkpreview/LinkPreviewApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.shivamkumarjha.linkpreview
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LinkPreviewApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
